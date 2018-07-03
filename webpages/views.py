1478624a98100df095cb5b7b60ad9c83cd017f36
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-07-03 05:35:42 -0700

    Changes

--- webpages/views.py
+++ webpages/views.py
@@ -1,19 +1,47 @@
+from __future__ import print_function
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 import requests
 import json
+from django.views import View
 
-def index(request, ) -> HttpResponse:
-    return redirect(events_hackathon_page)
+class IndexView(View):
 
-def events_page(request, ) -> HttpResponse:
-    return redirect(events_hackathon_page)
+    def get(self, request) -> HttpResponse:
+        return redirect("hackathon_page")
 
-def events_hackathon_page(request, ) -> HttpResponse:
-    return render(request, "pages/hackathon.html", context=None)
+class EventsView(View):
 
-def register_page(request, ) -> HttpResponse: 
-    return render(request, "pages/register.html", context=None)
+    def get(self, request) -> HttpResponse:
+        return redirect("hackathon_page")
 
-def login_page(request, ) -> HttpResponse:
-    return render(request, "pages/login.html", context=None)
+class HackathonView(View):
+    
+    def get(self, request) -> HttpResponse:
+        return render(request, "pages/hackathon.html", context=None)
+
+class RegisterView(View):
+
+    def get(self, request) -> HttpResponse:
+        return render(request, "pages/register.html", context=None)
+
+    def post(self, request) -> HttpResponse:
+        form = request.POST
+        error = 
+        if form["password"] == form["password_again"]:
+            try:
+                #promise = auth.create_user_with_email_and_password(form["email"],form["password"])
+            except Exception as e:
+                print(e)
+                error = "critical"
+        else:
+            error = "passwords_dont_match"
+        return render(request, "pages/register.html", {"error":error})
+
+class Loginiew(View):
+
+    def get(self, request) -> HttpResponse:
+        return render(request, "pages/login.html", context=None)
+    
+    def post(self, request) -> HttpResponse:
+        pass
