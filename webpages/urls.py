68cf2507d04fdb8305b556c0c32bf76b6b211cfe
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-07-07 21:18:57 -0700

    Added name fields for some urls for better internal referencing

--- webpages/urls.py
+++ webpages/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index),
-    path('events/', views.events_page, name="events_page"),
-    path('events/hackathon/', views.events_hackathon_page, name="hackathon_page"),
-    path('register/', views.register_page),
-    path('login/', views.login_page),
+    path('', views.IndexView.as_view(), name="index"),
+    path('events/', views.EventsView.as_view(), name="events_page"),
+    path('events/hackathon/', views.HackathonView.as_view(), name="hackathon_page"),
+    path('register/', views.RegisterView.as_view()),
+    path('login/', views.Loginiew.as_view()),
 ]
