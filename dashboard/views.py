431a46afc148adf1cbb6dd3f1f5826597ebcfd70
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-07-07 21:17:06 -0700

    Added some basic code to start dashboard views with fiirebase

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import firebase_admin
+from django.views import View
+from firebase_admin import credentials
+from firebase_admin import auth
 
-# Create your views here.
+cred = credentials.Certificate('path/to/serviceAccountKey.json')
+default_app = firebase_admin.initialize_app(cred)
+
+class DashboardView(View):
+    
+    def get(self, request) -> HttpResponse:
+        return redirect("events_dashboard")
+
+class EventsView(View):
+
+    def get(self, request) -> HttpResponse:
+        pass
