dbb366648f7b5a6167d7203b2db267075042d45d
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-07-14 10:27:07 -0700

    fixed url issue

--- webpages/urls.py
+++ webpages/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('events/', views.EventsView.as_view(), name="events_page"),
-    path('events/2019/', views.2019Views)
+    path('events/2019/', views.NineteenView.as_view(), name="2019_events"),
     path('events/2019/hackathon/', views.HackathonView.as_view(), name="hackathon_page"),
-    path('register/', views.RegisterView.as_view()),
-    path('login/', views.Loginiew.as_view()),
+    path('register/', views.RegisterView.as_view(), name="register"),
+    path('login/', views.Loginiew.as_view(), name="login"),
 ]
