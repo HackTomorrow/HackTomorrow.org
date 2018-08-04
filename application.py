7886597635109976dc702c473cd0be50dbd3c359
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-08-04 04:07:41 -0500

    setup login, join routes and new requirements

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, request
+
+# FIREBASE ADMIN SETUP
 
 application = Flask(__name__)
 
@@ -6,9 +8,23 @@ application = Flask(__name__)
 def index():
     return redirect("/hackathon")
 
+
 @application.route("/hackathon")
 def hackathon():
     return render_template("hackathon.html")
+
+
+@application.route("/login")
+def login():
+    if request.args.get("token"):
+        pass
+    return render_template("login.html")
+
+
+@application.route("/join")
+def join():
+    return render_template("join.html")
+
 
 if __name__ == "__main__":
     application.run()
