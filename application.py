019c6c812bda7688562a053b9363dc1a6916b040
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-08-06 15:16:38 -0500

    added login-new

--- application.py
+++ application.py
@@ -1,8 +1,26 @@
-from flask import Flask, render_template, redirect, request
-
-# FIREBASE ADMIN SETUP
+from flask import Flask, render_template, redirect, request, url_for
+import firebase_admin
+from firebase_admin import credentials, auth, firestore
+import os
 
 application = Flask(__name__)
+
+secret = os.getenv("TCF_ACCOUNTS_SECRET")
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+if not os.path.isfile("key.json"):
+    session = boto3.session.Session(
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
+    )
+    session.resource("s3").Bucket("tcf-accounts-key").download_file(
+        "key.json", "key.json"
+    )
+
+cred = credentials.Certificate("key.json")
+default_app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 @application.route("/")
 def index():
@@ -16,18 +34,64 @@ def hackathon():
 
 @application.route("/login")
 def login():
-    token = request.args.get("token")
-    if token:
-        # sign in
-        # add new permissions to database
-        pass
     return render_template("login.html")
+
+
+@application.route("/login/new")
+def login_new():
+    token = request.args.get("token")
+    if not token:
+        redirect(url_for("login"))
 
 
 @application.route("/join")
 def join():
     return render_template("join.html")
 
+
+@application.route("/set-claims", methods=["POST"])
+def set_claims():
+    try:
+        token = request.form["token"]
+    except KeyError as e:
+        pass
+    except Exception as e:
+        pass
+    else:
+        try:
+            decoded_token: dict = auth.verify_id_token(token)
+        except Exception as e:
+            pass
+        else:
+            try:
+                uid = decoded_token["uid"]
+            except KeyError as e:
+                pass            
+            except Exception as e:
+                pass
+            else:
+                try:
+                    doc_data: dict = db.collection("accounts").document(uid).get().to_dict()
+                except TypeError as e:
+                    pass
+                except Exception as e:
+                    pass
+                else:
+                    try:
+                        permissions: dict = doc_data["permissions"]
+                    except TypeError as e:
+                        pass
+                    except KeyError as e:
+                        pass
+                    except Exception as e:
+                        pass
+                    else:
+                        try:
+                            auth.set_custom_user_claims(uid, permissions)
+                        except Exception as e:
+                            pass
+                        else:
+                            pass
 
 if __name__ == "__main__":
     application.run()
