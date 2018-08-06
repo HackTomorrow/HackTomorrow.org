b2b292005c6ce6bd9a830536d14e79a339035ef0
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-08-06 15:25:11 -0500

    completed set-claims method

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 import os
+import boto3
 
 application = Flask(__name__)
 
@@ -54,44 +55,54 @@ def set_claims():
     try:
         token = request.form["token"]
     except KeyError as e:
-        pass
+        return jsonify(
+                    {"statusCode": 406, "message": "KeyError Exception" + str(e)}
+                )
     except Exception as e:
         pass
     else:
         try:
             decoded_token: dict = auth.verify_id_token(token)
         except Exception as e:
-            pass
+            return jsonify({"statusCode": 505, "message": "Exception" + str(e)})
         else:
             try:
                 uid = decoded_token["uid"]
             except KeyError as e:
-                pass            
+                return jsonify(
+                    {"statusCode": 406, "message": "KeyError Exception" + str(e)}
+                )            
             except Exception as e:
-                pass
+                return jsonify({"statusCode": 505, "message": "Exception" + str(e)})
             else:
                 try:
                     doc_data: dict = db.collection("accounts").document(uid).get().to_dict()
                 except TypeError as e:
-                    pass
+                    return jsonify(
+                        {"statusCode": 406, "message": "TypeError Exception" + str(e)}
+                    )
                 except Exception as e:
-                    pass
+                    return jsonify({"statusCode": 505, "message": "Exception" + str(e)})pass
                 else:
                     try:
                         permissions: dict = doc_data["permissions"]
                     except TypeError as e:
-                        pass
+                        return jsonify(
+                            {"statusCode": 406, "message": "TypeError Exception" + str(e)}
+                        )
                     except KeyError as e:
-                        pass
+                        return jsonify(
+                            {"statusCode": 406, "message": "KeyError Exception" + str(e)}
+                        )
                     except Exception as e:
-                        pass
+                        return jsonify({"statusCode": 505, "message": "Exception" + str(e)})
                     else:
                         try:
                             auth.set_custom_user_claims(uid, permissions)
                         except Exception as e:
-                            pass
+                            return jsonify({"statusCode": 505, "message": "Exception" + str(e)})
                         else:
-                            pass
+                            return jsonify({"statusCode": 202, "message": "operation successful"})
 
 if __name__ == "__main__":
     application.run()
